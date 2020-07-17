a85faba9f4989cd044758d24c4685e157e0f6b3d
Hector Alvarez <h-alvarez@wiu.edu>
2020-07-17 00:01:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,8 @@
 def join_nested_strings(src)
-  
+  string_array = []
+  row_index = 0 
+  while row_index < src.length do 
+    element_index = 0 
+    while element_index < src[row_index]. is_a? String 
+    string_array << src[row_index][element_]
 end
